b8b3ee588f9adaf6c20872643d45b9f6acf782c9
Mohammad Dehghan <md.unicorn@gmail.com>
2020-07-10 05:04:05 +0430

    WIP - Add MessageEditor component with required capabilities
    I've problems with updating message text after adding an emoji
    Image attachment is not implemented.

---
+++ MyChatApp/Client/wwwroot/js/main.js
@@ -0,0 +1,35 @@
+﻿function focusElement(element) {
+    element.focus();
+}
+
+function createEmojiPicker(button, editable) {
+    const picker = new EmojiButton();
+    picker.on('emoji', emoji => {
+        insertAtCursor(editable, emoji);
+        setTimeout(() => editable.focus(), 0);
+    });
+
+    button.addEventListener('click', () => {
+        picker.togglePicker(button);
+    });
+}
+
+function insertAtCursor(element, text) {
+    //IE support
+    if (document.selection) {
+        element.focus();
+        sel = document.selection.createRange();
+        sel.text = text;
+    }
+    //MOZILLA and others
+    else if (element.selectionStart || element.selectionStart == '0') {
+        var startPos = element.selectionStart;
+        var endPos = element.selectionEnd;
+        element.value = element.value.substring(0, startPos)
+            + text
+            + element.value.substring(endPos, element.value.length);
+        element.selectionStart = element.selectionEnd = startPos + text.length;
+    } else {
+        element.value += text;
+    }
+}
